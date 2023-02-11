d75ba16fa251b82dddfaeaa13027739baa6d7f25
xubai2001 <xubairzhou@gmail.com>
2023-02-11 18:28:48 +0800

    remove useless file && use script to convert yaml to list format

---
+++ yaml2list.py
@@ -0,0 +1,21 @@
+import yaml
+
+
+def load_yaml(filename) -> list:
+    with open(filename, mode="r") as f:
+        content = f.read()
+    rules_dict = yaml.safe_load(content)
+    return [i + "\n" for i in rules_dict["payload"]]
+
+def write_list(rules: list, filename: str):
+    with open(filename, mode="w") as f:
+        f.writelines(rules)
+
+def main():
+    filenames = ["proxy", "direct", "custom_emby", "us", "video"]
+    for name in filenames:
+        rules = load_yaml(f"{name}.yaml")
+        write_list(rules, f"{name}.list")
+
+if __name__ == "__main__":
+    main()
