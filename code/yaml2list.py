91f62d39e65250389be5d1a18fd9139da0692009
xubai2001 <xubairzhou@gmail.com>
2023-02-11 22:17:59 +0800

    Organize dir

--- code/yaml2list.py
+++ code/yaml2list.py
@@ -13,12 +13,11 @@ def write_list(rules: list, filename: str):
         f.writelines(rules)
 
 def main():
+    root_dir = os.getcwd()
     filenames = ["proxy", "direct", "custom_emby", "us", "video", "Bilibili"]
     for name in filenames:
-        rules = load_yaml(f"{name}.yaml")
-        write_list(rules, f"{name}.list")
-    print(os.getcwd())
-    print("write complete")
+        rules = load_yaml(f"{root_dir}/Clash/{name}.yaml")
+        write_list(rules, f"{root_dir}/Surge/{name}.list")
 
 if __name__ == "__main__":
     main()
